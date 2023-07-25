b31973392420cedefc8b940302045813a9607ba2
Mieu Teacher <mieuteacher@gmail.com>
2023-07-25 14:29:37 +0700

    connect mysql

--- src/server.js
+++ src/server.js
@@ -6,6 +6,10 @@ dotenv.config();
 import express from 'express';
 const server = express();
 
+/* Connect MySQL */
+import {mysqlConnect} from './databases/mySQL';
+mysqlConnect();
+
 server.listen(process.env.SERVER_PORT, () => {
     console.log(`Server đã chạy tại: ${process.env.SEVER_HOST}:${process.env.SERVER_PORT}/`)
 })
