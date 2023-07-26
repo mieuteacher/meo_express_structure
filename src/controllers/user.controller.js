fe8309b0ceaa3755845ed5a3ba0d9240ae1d6e64
Mieu Teacher <mieuteacher@gmail.com>
2023-07-26 10:53:47 +0700

    get user detail

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,18 +4,34 @@ module.exports = {
     getUsers: async function(req, res) {
         /* Get User By Id */
         if(req.query.userId) {
-            let result = await userModel.getUserById(req.query.userId);
-            if (result.status) {
-                return res.status(200).json(
-                    {
+            if (Boolean(req.query.detail)) {
+                let result = await userModel.getUserDetailById(req.query.userId);
+                if (result.status) {
+                    return res.status(200).json(
+                        {
+                            message: result.message,
+                            data: result.data
+                        }
+                    )
+                }else {
+                    return res.status(500).json({
                         message: result.message,
-                        data: result.data
-                    }
-                )
+                    })
+                }
             }else {
-                return res.status(500).json({
-                    message: result.message,
-                })
+                let result = await userModel.getUserById(req.query.userId);
+                if (result.status) {
+                    return res.status(200).json(
+                        {
+                            message: result.message,
+                            data: result.data
+                        }
+                    )
+                }else {
+                    return res.status(500).json({
+                        message: result.message,
+                    })
+                }
             }
         }
 
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -57,8 +57,77 @@ module.exports = {
                  return resolve(
                      {
                          status: true,
-                         message: "Get users thành công!",
+                         message: "Get users by idthành công!",
                          data: result
+                     }
+                 )
+             })
+        }).catch(err => {
+             //console.log("Promise gặp lỗi cú pháp!", err)
+             return {
+                 status: false,
+                 message: "Lỗi không xác định!"
+             }
+        })
+     },
+    getUserDetailById: function(userId) {
+        return new Promise((resolve, reject) => {
+         
+             let queryString = `
+                SELECT users.*, user_address.id as addressId, user_address.name as addressName, user_address.provinceId, user_address.wardId
+                FROM users
+                LEFT JOIN user_address on users.id = user_address.userId
+                WHERE users.id = ${userId}
+             `
+ 
+             mySQL.query(queryString, (err, result) => {
+                 if(err) {
+                     return resolve(
+                         {
+                             status: false,
+                             message: "Lỗi trong quá trình truy vấn!"
+                         }
+                     )
+                 }
+                 //console.log("result", result)
+                let user;
+                for (let i in result) {
+                    if (!user) {
+                        user = {
+                            id: result[i].id,
+                            name: result[i].name,
+                            email: result[i].email,
+                            address: 
+                                result[i].addressId 
+                                ? 
+                                [
+                                    {
+                                        id:   result[i].addressId,
+                                        name: result[i].addressName,
+                                        provinceId: result[i].provinceId,
+                                        wardId: result[i].wardId,
+                                    }
+                                ]
+                                :
+                                []
+                        }
+                        continue;
+                    }
+                    user.address.push(
+                        {
+                            id:   result[i].addressId,
+                            name: result[i].addressName,
+                            provinceId: result[i].provinceId,
+                            wardId: result[i].wardId,
+                        }
+                    )
+                }
+
+                 return resolve(
+                     {
+                         status: true,
+                         message: "Get users thành công!",
+                         data: user
                      }
                  )
              })
@@ -81,22 +150,22 @@ module.exports = {
             let queryString = `
                 INSERT INTO users SET ?
              `
-             mySQL.query(queryString, newUser, (err, result) => {
+             mySQL.query(queryString, newUser, async (err, result) => {
                  if(err) {
-                    console.log("lỗi truy vấn", err)
+                    //console.log("lỗi truy vấn", err)
                      return resolve(
                          {
                              status: false,
-                             message: "Lỗi trong quá trình truy vấn!"
+                             message: err.code == "ER_DUP_ENTRY" ? "Email đã tồn tại" : "Lỗi truy vấn!"
                          }
                      )
                  }
- 
+                 let insertData = await this.getUserById(result.insertId);
                  return resolve(
                      {
                          status: true,
-                         message: "Get users thành công!",
-                         data: result
+                         message: "Insert user thành công!",
+                         data: insertData.status ? insertData.data : {}
                      }
                  )
              })
@@ -109,3 +178,34 @@ module.exports = {
         })
      },
 }
+
+
+
+
+// user = {
+//     id: 6,
+//     name: "Hương",
+//     email: "huong@gmail.com",
+//     address: [
+//         {
+//             id: 1,
+//             name: "Nhà Riêng",
+//             provinceId: 1,
+//             wardId: 5
+//         },
+//         {
+//             id: 2,
+//             name: "Công Ty",
+//             provinceId: 1,
+//             wardId: 5
+//         }
+//     ]
+// }
+
+
+// user2 = {
+//     id: 5,
+//     name: "Tài",
+//     email: "tai@gmail.com",
+//     address: []
+// }
